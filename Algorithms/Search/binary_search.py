cab5dd41a5ecfb8be36e184cac111aeadcc6cee7
Jayson Zhou <jaysonz6879@gmail.com>
2022-03-19 14:18:20 +0000

    [Update] adjust the binary search code.

--- Algorithms/Search/binary_search.py
+++ Algorithms/Search/binary_search.py
@@ -1,19 +1,20 @@
 # Sorry that was a test.
 
-def binary_search(arr, k):
+def binary_search(_arr, _k):
     left = 0
-    right = len(arr) - 1
+    right = len(_arr) - 1
     while left < right:
         mid = (left + right) // 2
-        if k < arr[mid]:
+        if _k < _arr[mid]:
             right = mid - 1
-        if k > arr[mid]:
+        if _k > _arr[mid]:
             left = mid + 1
         else:
             return mid
     return -1
 
+
 arr = [1, 4, 5, 7, 19, 22, 34, 45, 50, 55, 60, 65, 70, 99]
-k = 50
+k = 55
 
 print(binary_search(arr, k))
