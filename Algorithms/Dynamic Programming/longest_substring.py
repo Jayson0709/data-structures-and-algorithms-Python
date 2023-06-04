43cdb082f56be127dafbb3a5bf5cb4a00a163e9c
JaysonZhou <jaysonz6879@gmail.com>
2023-06-04 13:04:03 +0800

    [Update] fix some code style issues.

--- Algorithms/Dynamic Programming/longest_substring.py
+++ Algorithms/Dynamic Programming/longest_substring.py
@@ -1,4 +1,5 @@
 # 给两个字符串 输出最长子序列的长度
+# Given two strings, output the longest substring's length
 import sys
 
 input_thing = []
@@ -18,12 +19,12 @@ print("The second string is : ", string2)
 def longest_substring(s1, s2):
     if len(s1) < len(s2):
         s1, s2 = s2, s1
-    maxstr = s1
-    substr_maxlen = max(len(s1), len(s2))
-    for sublen in range(substr_maxlen, -1, -1):
-        for i in range(substr_maxlen - sublen + 1):
-            if maxstr[i: i + sublen] in s2:
-                return maxstr[i: i + sublen]
+    max_str = s1
+    substr_max_len = max(len(s1), len(s2))
+    for sub_len in range(substr_max_len, -1, -1):
+        for i in range(substr_max_len - sub_len + 1):
+            if max_str[i: i + sub_len] in s2:
+                return max_str[i: i + sub_len]
 
 
 if __name__ == "__main__":
